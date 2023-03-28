1a74225f247c7f3b3dc5d7b2b285f80a3a02e374
kemcrypt <kemiola89@yahoo.com>
2023-03-28 11:40:04 +0100

    Update 1-safe_print_integer.py

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 def safe_print_integer(value):
     """Print an integer with "{:d}".format().
 
@@ -12,7 +13,3 @@ def safe_print_integer(value):
     """
     try:
         print("{:d}".format(value))
-        return (True)
-    except (TypeError, ValueError):
-        return (False)
-
